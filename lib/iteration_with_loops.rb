6fafce77e9603b211018c198d4d9db97b7790d6b
Mathew E Newton <elijah2895@hotmail.com>
2021-02-10 01:00:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,7 @@ def find_even_values(src)
     element_index = 0
     while element_index < src[row_index].count do
       p src[row_index][element_index]
+      element_index += 1
     end
   end
 end
